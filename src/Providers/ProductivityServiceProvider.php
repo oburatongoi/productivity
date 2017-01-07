5dfc6197a1bd6926c3dad1df7f136957df2e1006
Obura Tongoi <obura@tongoi.com>
2017-01-06 23:48:37 -0800

    Routing changes

--- src/Providers/ProductivityServiceProvider.php
+++ src/Providers/ProductivityServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Oburatongoi\Productivity\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route;
 
 class ProductivityServiceProvider extends ServiceProvider
 {
@@ -17,8 +16,6 @@ class ProductivityServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'productivity');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        $this->defineRoutes();
 
         $this->publishes([
             __DIR__.'/../resources/views' => resource_path('views/vendor/productivity')
@@ -44,32 +41,4 @@ class ProductivityServiceProvider extends ServiceProvider
         $this->app->register('Baum\Providers\BaumServiceProvider');
     }
 
-    /**
-     * Define the Package's routes.
-     *
-     * @return void
-     */
-    protected function defineRoutes()
-    {
-        $this->defineFakeIdBindings();
-        // If the routes have not been cached, we will include them in a route group
-        // so that all of the routes will be conveniently registered to the given
-        // controller namespace. After that we will load the Spark routes file.
-        if (! $this->app->routesAreCached()) {
-            Route::group([
-                'namespace' => 'Oburatongoi\Productivity\Http\Controllers'],
-                function ($router) {
-                    require __DIR__.'/../Http/routes.php';
-                }
-            );
-        }
-    }
-
-    protected function defineFakeIdBindings()
-    {
-        Route::fakeIdModel('folder', 'Oburatongoi\Productivity\Folder');
-        Route::fakeIdModel('list', 'Oburatongoi\Productivity\List');
-        Route::fakeIdModel('note', 'Oburatongoi\Productivity\Note');
-        Route::fakeIdModel('goal', 'Oburatongoi\Productivity\Goal');
-    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
-// Route::fakeIdModel('folder', 'Oburatongoi\Productivity\Folder');
-// Route::fakeIdModel('list', 'Oburatongoi\Productivity\List');
-// Route::fakeIdModel('note', 'Oburatongoi\Productivity\Note');
-// Route::fakeIdModel('goal', 'Oburatongoi\Productivity\Goal');
+Route::fakeIdModel('folder', 'Oburatongoi\Productivity\Folder');
+Route::fakeIdModel('list', 'Oburatongoi\Productivity\List');
+Route::fakeIdModel('note', 'Oburatongoi\Productivity\Note');
+Route::fakeIdModel('goal', 'Oburatongoi\Productivity\Goal');
 
 
 Route::get('productivity', function(){
     echo 'works';
 });
 
-Route::resource('folder', 'OburaTongoi\Productivity\Http\Controllers\FolderController');
+Route::group(['namespace' => 'Oburatongoi\Productivity\Http\Controllers'], function () {
+    Route::resource('folder', 'FolderController');
+});
