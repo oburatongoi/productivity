87a0ad36f3480985a533ab7aefb6f1146bb7bb05
Obura Tongoi <obura@tongoi.com>
2017-01-08 08:24:13 -0800

    Placed routes in productivity prefix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
-Route::fakeIdModel('folder', 'Oburatongoi\Productivity\Folder');
-Route::fakeIdModel('list', 'Oburatongoi\Productivity\List');
-Route::fakeIdModel('note', 'Oburatongoi\Productivity\Note');
-Route::fakeIdModel('goal', 'Oburatongoi\Productivity\Goal');
+Route::group(['prefix' => 'productivity'], function () {
+
+    Route::fakeIdModel('folder', 'Oburatongoi\Productivity\Folder');
+    Route::fakeIdModel('list', 'Oburatongoi\Productivity\List');
+    Route::fakeIdModel('note', 'Oburatongoi\Productivity\Note');
+    Route::fakeIdModel('goal', 'Oburatongoi\Productivity\Goal');
 
 
-Route::get('productivity', function(){
-    echo 'works';
-});
+    Route::get('/', function(){
+        echo 'prefix works';
+    });
 
-Route::group(['namespace' => 'Oburatongoi\Productivity\Http\Controllers'], function () {
-    Route::resource('folders', 'FolderController', ['except' => ['create', 'edit']]);
-    Route::resource('goals', 'GoalController', ['except' => ['create', 'edit']]);
-    Route::resource('notes', 'NoteController', ['except' => ['create', 'edit']]);
-    Route::resource('lists', 'ChecklistController', ['except' => ['create', 'edit']]);
+    Route::group(['namespace' => 'Oburatongoi\Productivity\Http\Controllers'], function () {
+        Route::resource('folders', 'FolderController', ['except' => ['create', 'edit']]);
+        Route::resource('goals', 'GoalController', ['except' => ['create', 'edit']]);
+        Route::resource('notes', 'NoteController', ['except' => ['create', 'edit']]);
+        Route::resource('lists', 'ChecklistController', ['except' => ['create', 'edit']]);
+    });
+
 });
