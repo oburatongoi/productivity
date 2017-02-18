4d23843b93cc8597249a10a16411fcc4192b5649
Obura Tongoi <obura@tongoi.com>
2017-02-18 07:51:57 -0800

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack')
+
+module.exports = {
+  // ... other options
+  plugins: [
+    // short-circuits all Vue.js warning code
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    // minify with dead-code elimination
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    // optimize module ids by occurrence count
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ]
+}
