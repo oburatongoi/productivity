242b99abb9ec63c040b3e093926d184f2c451934
Obura Tongoi <obura@tongoi.com>
2017-10-18 16:54:37 -0700

    Show only list items from task lists in task view

--- src/Repositories/ChecklistItems.php
+++ src/Repositories/ChecklistItems.php
@@ -20,46 +20,30 @@ class ChecklistItems implements ChecklistItemsInterface {
 
     public function pendingForUser(User $user)
     {
-        return $user->items()->where([
-            ['checked_at', null],
-            ['list_type', 'ta']
-        ])->with('checklist.folder')->get();
+        // return $user->items()->where([
+        //     ['checked_at', null],
+        //     ['list_type', 'ta']
+        // ])->with('checklist.folder')->get();
 
-        // $q = DB::table('productivity_checklist_items')
-        //             ->where([
-        //                 ['productivity_checklist_items.deleted_at', null],
-        //                 ['productivity_checklist_items.checked_at', null]
-        //             ])
-        //             ->join('productivity_checklists', function ($join) {
-        //                 $join->on('productivity_checklist_items.checklist_id', '=', 'productivity_checklists.id')
-        //                     ->where([
-        //                         ['productivity_checklists.deleted_at', null],
-        //                         ['productivity_checklists.list_type', 'ta']
-        //                     ]);
-        //             })
-        //             ->leftJoin('productivity_folders', function ($join) {
-        //                 $join->on('productivity_checklists.folder_id', '=', 'productivity_folders.id')
-        //                 ->where('productivity_folders.deleted_at', null);
-        //             })
-        //             ->select(
-        //                 'productivity_checklist_items.*',
-        //                 'productivity_checklists.title as checklist_title',
-        //                 'productivity_checklists.fake_id as checklist_fake_id',
-        //                 'productivity_folders.name as folder_name',
-        //                 'productivity_folders.fake_id as folder_fake_id'
-        //             )->get();
-        //
-        //             dd($q);
+        return $user->items()
+            ->whereNull('checked_at')
+            ->with('checklist.folder')
+            ->whereHas('checklist', function($query) {
+                $query->where('list_type', 'ta');
+            })
+            ->get();
 
     }
 
     public function pendingForFolder(Folder $folder)
     {
-        return $folder->items()->where([
-            ['checked_at', null]
-        ])->with(['checklist' => function($query) {
-            $query->where('list_type', 'ta');
-        }, 'checklist.folder'])->get();
+        return $folder->items()
+            ->whereNull('checked_at')
+            ->with('checklist.folder')
+            ->whereHas('checklist', function($query) {
+                $query->where('list_type', 'ta');
+            })
+            ->get();
 
     }
 
